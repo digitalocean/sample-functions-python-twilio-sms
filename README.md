d341249b01a7a5eaf7705df9caa5aa768da906a6
Shivani Kumar <shivanik1920@gmail.com>
2022-07-11 15:59:26 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sample Function: Python "Twilio SMS"
+
+## Introduction
+
+This repository contains a sample Twilio SMS function written in Python. You are able to send an email using Sendgrid's API.
---
+++ packages/sample/sms/sms.py
@@ -0,0 +1,40 @@
+import os
+from twilio.rest import Client
+from twilio.base.exceptions import TwilioRestException
+
+def valid_number(number, client):
+    try: 
+        client.lookups.phone_numbers(number).fetch(type = "carrier")
+        return True
+    except TwilioRestException as e:
+        if e == 21606:
+            return False
+        else:
+            raise e
+
+
+def main(args):
+    sid = os.getenv('TWILIO_ACCOUNT_SID')
+    token = os.getenv('TWILIO_AUTH_TOKEN')
+    number = args.get("number")
+    user_to = args.get("receiver number")
+    body = args.get("body", "this was sent from a twilio sms number")
+
+    if not number:
+        return {"body" : "no number provided"}
+    if not user_to:
+        return {"body" : "no receiver phone number provided"}
+    if not body:
+        return {"body" : "no message provided"}
+
+    client = Client(sid, token)
+    if (valid_number(number, client) == True) & (valid_number(user_to, client) == True):
+        client.messages.create(
+            body = body,
+            from_ = number,
+            to = user_to
+        )
+        return {"body" : "message sent"}
+    else:
+        return {"body" : "phone numbers provided are not twilio verified numbers"}
+
