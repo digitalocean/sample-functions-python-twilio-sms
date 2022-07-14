e86c13fc67afd1ed86799bd431486bb5886d4301
Shivani Kumar <shivanik1920@gmail.com>
2022-07-14 17:20:34 -0400

    Adding comments

--- packages/sample/sms/__main__.py
+++ packages/sample/sms/__main__.py
@@ -3,6 +3,16 @@ from twilio.rest import Client
 from twilio.base.exceptions import TwilioRestException
 
 def valid_number(number, client):
+    '''
+    Returns true if the number given is a valid twilio phone number.
+
+        Parameters:
+            number: Phone number provided to validate
+            client: Connects to Twilio API
+
+        Returns:
+            boolean: True if valid phone number, false if invalid phone number
+    '''
     try: 
         client.lookups.phone_numbers(number).fetch(type = "carrier")
         return True
@@ -14,6 +24,16 @@ def valid_number(number, client):
 
 
 def main(args):
+    '''
+    Takes in the phone numbers and message to send an sms using Twilio, 
+    returns a json response letting the user know if the message sent or failed to send.
+
+        Parameters:
+            args: Contains the from phone number, to phone number, and message to send
+
+        Returns:
+            json body: Json response if the message sent successfully or if an error happened
+    '''
     sid = os.getenv('TWILIO_ACCOUNT_SID')
     token = os.getenv('TWILIO_AUTH_TOKEN')
     number = args.get("from")
